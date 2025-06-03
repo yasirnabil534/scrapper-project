74c6380a9ab4ed0ade1fb4c85db1f35ccc23a532
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2025-06-03 14:19:31 +0600

    added access machanism and apply token from gmail

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import createError from '../common/error';
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import createError from "../common/error.js";
+import { getAccess, getOauth2Callback } from "../get-access/access.js";
+import main from "../main.js";
 
 const app = express();
 
-app.use('/webhook', bodyParser.raw({ type: '*/*' }));
+app.use("/webhook", bodyParser.raw({ type: "*/*" }));
 // app.use(bodyParser.raw({ type: '*/*' }))
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,7 +15,14 @@ app.use(cors());
 // * Logger middleware
 app.use((req, res, next) => {
   res.on("finish", () => {
-    console.log(req.method, req.hostname, req.path, res.statusCode, res.statusMessage, new Date(Date.now()))
+    console.log(
+      req.method,
+      req.hostname,
+      req.path,
+      res.statusCode,
+      res.statusMessage,
+      new Date(Date.now())
+    );
   });
   next();
 });
@@ -22,19 +31,22 @@ app.use((req, res, next) => {
 app.get("/", (req, res, next) => {
   try {
     res.status(200).json({ messge: "Connection established" });
-  } catch (err) {
-    next(createError());
+  } catch (err: any) {
+    next(createError(err.status, err.message));
   }
 });
 
 // ~ Router starts here
+app.get("/auth", getAccess as any);
+app.get("/oauth2callback", getOauth2Callback as any);
+app.get("/api/expedia", main);
 
 // * GLobal error handle middleware
-app.use((err, req, res, next) => {
+app.use((err: any, req: any, res: any, next: any) => {
   if (res.headersSent) {
     return next(err);
   }
-  
+
   const errMessage = err.message || "Something went wrong";
   const errStatus = err.status || 500;
   return res.status(errStatus).json({
